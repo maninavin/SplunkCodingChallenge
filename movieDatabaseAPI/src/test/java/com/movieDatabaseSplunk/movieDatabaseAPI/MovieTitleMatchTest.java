e4f2d76bf7ad6b1f38cb229f97ac7008b151924f
maninavin <gn.manikandan@gmail.com>
2018-05-15 21:26:12 -0700

    Modified Title match test case

---
+++ movieDatabaseAPI/src/test/java/com/movieDatabaseSplunk/movieDatabaseAPI/MovieTitleMatchTest.java
@@ -0,0 +1,43 @@
+package com.movieDatabaseSplunk.movieDatabaseAPI;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+import com.movieDatabaseSplunk.movieDatabaseAPI.*;
+
+import groovyjarjarantlr.collections.List;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.testng.Assert.assertTrue;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Set;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class MovieTitleMatchTest {
+
+	@Test
+	public void testMoviesTitleMatch() {
+
+		Response res = ReusableMethods.getResponseData();
+		JsonPath js = ReusableMethods.rawtoJson(res);
+
+		assertTrue(MoviesInformationHelper.verifyMoviesTitlesMatch(js),
+				"There are no two movies whose title contains title of another.");
+
+	}
+
+	
+}
